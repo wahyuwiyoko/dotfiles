e06c4b4bd72274c66fed11d355fa783088dcbc25
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-11-16 12:30:26 +0800

    Remove unused software for setup scripts

--- setup/apps.sh
+++ setup/apps.sh
@@ -6,17 +6,11 @@
 sudo apt install -y alacritty firefox-esr lf mpv cmus zathura pass newsboat
 
 # Tools
-sudo apt install -y ripgrep fd-find plocate tmux yt-dlp fzf intel-gpu-tools \
-  shellcheck libimage-exiftool-perl trash-cli tlp playerctl tealdeer
-
-# Update database for plocate package
-sudo updatedb
+sudo apt install -y ripgrep fd-find tmux yt-dlp fzf intel-gpu-tools \
+  shellcheck libimage-exiftool-perl trash-cli playerctl tealdeer
 
 # Choose default terminal emulator
 sudo update-alternatives --config x-terminal-emulator
-
-# Enable TLP
-sudo systemctl enable tlp
 
 # Create local binary directory
 mkdir -p "$HOME/.local/bin"
--- setup/development.sh
+++ setup/development.sh
@@ -16,20 +16,11 @@ extract_tarballs "https://go.dev/dl/go1.20.7.linux-amd64.tar.gz"
 # Hugo
 extract_tarballs "https://github.com/gohugoio/hugo/releases/download/v0.116.1/hugo_0.116.1_linux-amd64.tar.gz"
 
-# Delta
-extract_tarballs "https://github.com/dandavison/delta/releases/download/0.16.5/delta-0.16.5-x86_64-unknown-linux-gnu.tar.gz"
-
-# Insomnia
-extract_tarballs "https://github.com/Kong/insomnia/releases/download/core%402023.4.0/Insomnia.Core-2023.4.0.tar.gz"
-
 # ngrok
 extract_tarballs "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz"
 
 # hurl
 extract_tarballs "https://github.com/Orange-OpenSource/hurl/releases/download/4.0.0/hurl-4.0.0-x86_64-linux.tar.gz"
-
-# mdBook
-extract_tarballs "https://github.com/rust-lang/mdBook/releases/download/v0.4.34/mdbook-v0.4.34-x86_64-unknown-linux-gnu.tar.gz"
 
 # xdg-ninja
 wget -P "$HOME/downloads/development" -c --show-progress \
