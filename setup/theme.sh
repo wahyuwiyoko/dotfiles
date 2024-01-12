66fa1de5300ffe2861739ac22ea6ce9bb2ae2570
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-01-12 13:57:56 +0800

    Change cursor theme to Rosé Pine

--- setup/theme.sh
+++ setup/theme.sh
@@ -7,18 +7,16 @@ extract_tarballs() {
   wget -c --show-progress -O - "$1" | tar -xzf - -C  "$HOME/downloads/themes"
 }
 
-# Rosé Pine GTK and icon theme
+# Rosé Pine GTK theme, icon theme, and cursor theme
 extract_tarballs "https://github.com/rose-pine/gtk/releases/download/v2.1.0/gtk3.tar.gz"
 extract_tarballs "https://github.com/rose-pine/gtk/releases/download/v2.1.0/rose-pine-icons.tar.gz"
-
-# Catppuccin cursor theme
 wget -P "$HOME/downloads/themes" -c --show-progress \
-  https://github.com/catppuccin/cursors/releases/download/v0.2.0/Catppuccin-Mocha-Dark-Cursors.zip
+  https://github.com/rose-pine/cursor/releases/download/v1.1.0/BreezeX-RosePine-Linux.tar.xz
 
 # Move theme, icon, and cursor theme in local directory
 mv "$HOME/downloads/themes/gtk3/rose-pine-gtk" "$HOME/.local/share/themes"
 mv "$HOME/downloads/themes/icons/rose-pine-icons" "$HOME/.local/share/icons"
-unzip "$HOME/downloads/themes/Catppuccin-Mocha-Dark-Cursors.zip" -d "$HOME/.local/share/icons"
+tar -xvf "$HOME/downloads/themes/BreezeX-RosePine-Linux.tar.xz" -C "$HOME/.local/share/icons"
 
 # Update the icon cache
 gtk-update-icon-cache
